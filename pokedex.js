8b3444d8a1e37d7b3db58d7eeb70edf638ccdc3b
Marcos Almeida <marcosalmeidaxd@gmail.com>
2022-12-07 17:05:09 -0300

    Atualização de CSS. Adicionado Cards com animação e Requisição com o Tipo do Pokémon

--- pokedex.js
+++ pokedex.js
@@ -1,11 +1,11 @@
 var quantidade = document.getElementById('quantidade');
-quantidade.addEventListener('keyup',()=>{
+quantidade.addEventListener('keyup', () => {
     pegaPokemons(quantidade.value);
 })
 pegaPokemons(2);
 function pegaPokemons(quantidade) {
-   
-    
+
+
     fetch('https://pokeapi.co/api/v2/pokemon?limit=' + quantidade)
         .then(response => response.json())
         .then(allpokemon => {
@@ -17,7 +17,7 @@ function pegaPokemons(quantidade) {
 
                 fetch(val.url).then(response => response.json())
                     .then(pokemonSingle => {
-                        pokemons.push({ nome: val.name, imagem: pokemonSingle.sprites.front_default });
+                        pokemons.push({ nome: val.name, imagem: pokemonSingle.sprites.front_default, tipo: pokemonSingle.types[0].type.name });
 
 
                         if (pokemons.length == quantidade) {
@@ -28,12 +28,14 @@ function pegaPokemons(quantidade) {
                                 pokemonBoxes.innerHTML += `
                             
                             
-                            <div class="pokemon-box">
-                            
-                            <p>`+ val.nome + ` <img src="`+ val.imagem + `"></p>
+                            <div class="card">
+                            <h2>`+ val.nome + ` <br> Tipo: ` +val.tipo+`
+                            <img src="` + val.imagem + `"></h2>  
+                                      
                             </div> <!-- /.pokemon-box -->
-                        
+                            
                             `
+                                console.log(val.tipo);
                             })
 
                         }
